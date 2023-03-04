831093f3f7c2240f1c975a94fb3231838aea8c02
Mihaly Papp <misi@pappmihaly.com>
2023-03-04 16:53:37 +0100

    add gke auth plugin

--- shell.nix
+++ shell.nix
@@ -3,6 +3,13 @@
 pkgs.mkShell {
   name = "personal-infra-env";
   buildInputs =
+  let
+    gke-auth-plugin =
+      if pkgs.system == "aarch64-darwin"
+      then pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin-darwin-arm
+      else pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin;
+    google-cloud = pkgs.google-cloud-sdk.withExtraComponents [ gke-auth-plugin ];
+  in
     with pkgs; [
       kube3d
       kubectl
@@ -11,6 +18,7 @@ pkgs.mkShell {
       jq
       jiq
       kubernetes-helm
-      google-cloud-sdk
+      google-cloud
     ];
 }
+
