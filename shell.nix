86e5ac9e9989dd8e559bf747e9bd77b90a62ad9c
Mihaly Papp <misi@pappmihaly.com>
2023-06-24 10:28:47 +0200

    inline gcloud

--- shell.nix
+++ shell.nix
@@ -1,15 +1,8 @@
-{pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
   name = "pet-projects-infra-env";
   buildInputs =
-  let
-    gke-auth-plugin =
-      if pkgs.system == "aarch64-darwin"
-      then pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin-darwin-arm
-      else pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin;
-    google-cloud = pkgs.google-cloud-sdk.withExtraComponents [ gke-auth-plugin ];
-  in
     with pkgs; [
       kube3d
       kubectl
@@ -18,7 +11,11 @@ pkgs.mkShell {
       jq
       jiq
       kubernetes-helm
-      google-cloud
+      (pkgs.google-cloud-sdk.withExtraComponents [
+        (if pkgs.system == "aarch64-darwin"
+        then pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin-darwin-arm
+        else pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin)
+      ])
       argocd
       yq-go
       kubeseal
